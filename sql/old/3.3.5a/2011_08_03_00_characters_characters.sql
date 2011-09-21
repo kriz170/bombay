ceeb8387c7b0790d716614d66ecc3948ee906b3f
chicos <yopy.arnoldy@yahoo.com>
2011-09-21 14:51:27 +0700

    Sql regrup Signed-off-by: chicos <yopy.arnoldy@yahoo.com>

--- sql/old/3.3.5a/2011_08_03_00_characters_characters.sql
+++ sql/old/3.3.5a/2011_08_03_00_characters_characters.sql
@@ -1,1 +1,2 @@
 ALTER TABLE `characters` ADD COLUMN `grantableLevels` tinyint(3) unsigned NOT NULL DEFAULT '0' AFTER `actionBars`;
+ALTER TABLE `account` CHANGE `mutetime` `mutetime` bigint(40) NOT NULL DEFAULT 0;
