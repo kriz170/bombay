77980357991f9abb6f2784a5803ba8a85ff3ff4b
chicos <yopy.arnoldy@yahoo.com>
2011-09-22 19:41:32 +0700

    Fix Double Crit damage Signed-off-by: chicos <yopy.arnoldy@yahoo.com>

--- src/server/game/Entities/Unit/Unit.cpp
+++ src/server/game/Entities/Unit/Unit.cpp
@@ -11281,7 +11281,12 @@ uint32 Unit::SpellCriticalDamageBonus(SpellInfo const* spellProto, uint32 damage
             crit_bonus += damage / 2;                       // for spells is 50%
             break;
     }
-
+    
+	// all these spells should have only 50% bonus damage on crit like a magic spells
+	if (spellProto->Id == 55078 || spellProto->Id == 61840 ||
+	   (spellProto->SpellFamilyName == SPELLFAMILY_HUNTER && spellProto->SpellFamilyFlags[0] & 0x4000))
+	    crit_bonus /= 2;
+		
     crit_mod += (GetTotalAuraMultiplierByMiscMask(SPELL_AURA_MOD_CRIT_DAMAGE_BONUS, spellProto->GetSchoolMask()) - 1.0f) * 100;
 
     if (victim)
