1225c8cc9c8b96b95b46872f85a0f5b5e20f111c
Xanvial <xanvial@gmail.com>
2012-02-07 05:38:25 +0700

    apply patch anticheat

--- src/server/game/Entities/Player/Player.h
+++ src/server/game/Entities/Player/Player.h
@@ -1086,6 +1086,8 @@ class Player : public Unit, public GridObject<Player>
         explicit Player (WorldSession* session);
         ~Player ();
 
+        //AnticheatData anticheatData;
+
         void CleanupsBeforeDelete(bool finalCleanup = true);
 
         static UpdateMask updateVisualBits;
