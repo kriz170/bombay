6873f194f86a8af469e77c3540252098a11836ca
Xanvial <xanvial@gmail.com>
2012-02-20 10:18:15 +0700

    tambah sql buat:
    - ganti jumlahd ropan VDW dan BP 10N/H jadi 2 (sebelumnya 3)
    - ganti requirement faksi equip2 toc (jadi bisa dipake semua faksi)
    - ganti drop rate SFS

---
+++ sql/custom/BP and VDW 10 drop 2 item.sql
@@ -0,0 +1,2 @@
+UPDATE `creature_loot_template` SET `maxcount`=2 WHERE `entry` IN (37970,38784) AND `item`=1;
+UPDATE `gameobject_loot_template` SET `maxcount`=2 WHERE `entry`IN (28052,28064) AND `item`=1;
---
+++ sql/custom/equip toc update.sql
@@ -0,0 +1,1 @@
+update item_template set FlagsExtra=0 where buyprice != 0 and itemlevel IN (232,245,258,272) and FlagsExtra IN (1,2);
