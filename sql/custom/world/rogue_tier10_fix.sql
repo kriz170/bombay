76039db8b37dae6e77b0802a03163f292e6f19d0
Xanvial <xanvial@gmail.com>
2012-02-21 14:19:19 +0700

    Fix Rogue set 2 Tier 10

--- sql/custom/world/rogue_tier10_fix.sql
+++ sql/custom/world/rogue_tier10_fix.sql
@@ -1,3 +1,6 @@
 -- Rogue T10 4p bonus
 DELETE FROM `spell_proc_event` WHERE `entry` = 70803;
 INSERT INTO `spell_proc_event` VALUES('70803', '0', '8', '4063232', '8', '0', '0', '0', '0', '0', '0');
+
+-- Rogue T10 2p bonus
+UPDATE spell_proc_event SET procFlags=0, `Cooldown`=1 WHERE entry= 70805;
