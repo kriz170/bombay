f67f3263cdc2aeff8cdc9bdc342b60246dffdce4
Xanvial <xanvial@gmail.com>
2012-03-09 20:35:46 +0700

    kelupaan

--- sql/custom/world/update_equip_toc.sql
+++ sql/custom/world/update_equip_toc.sql
@@ -1,1 +1,2 @@
 update item_template set FlagsExtra=0 where buyprice != 0 and itemlevel IN (232,245,258,272) and FlagsExtra IN (1,2);
+update item_template set allowablerace=-1 where allowablerace IN (690,1101) and itemlevel IN (232,245,258,272);
