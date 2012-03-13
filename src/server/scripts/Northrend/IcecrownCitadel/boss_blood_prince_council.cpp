ff359739dd30584364816288044607ce7dc948c2
Xanvial <xanvial@gmail.com>
2012-03-13 23:05:24 +0700

    Change Shadow prison dummy from cast to addaura

--- src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
+++ src/server/scripts/Northrend/IcecrownCitadel/boss_blood_prince_council.cpp
@@ -402,7 +402,7 @@ class boss_prince_keleseth_icc : public CreatureScript
                 if (IsHeroic())
                 {
                     me->AddAura(SPELL_SHADOW_PRISON, me);
-                    DoCast(me, SPELL_SHADOW_PRISON_DUMMY);
+                    me->AddAura(SPELL_SHADOW_PRISON_DUMMY, me);
                 }
             }
 
