189218e52979ad619c5ae82dc6005e15fffdbcf1
krishna <krenz_37@yahoo.com>
2012-03-18 09:39:13 +0700

    kelupaan sql arena

---
+++ sql/custom/world/arena_announcer.sql
@@ -0,0 +1,2 @@
+UPDATE `trinity_string` SET `content_default`='|cffff0000[Arena Queue Announcer]:|r %s -- Joined : %ux%u |r' WHERE `entry`=718;
+UPDATE `trinity_string` SET `content_default`='|cffff0000[Arena Queue Announcer]:|r %s -- Exited : %ux%u |r' WHERE `entry`=719;
