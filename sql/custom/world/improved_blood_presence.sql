a79f5db9c9fa9c66e8734675df40c0ad9431a3d0
Xanvial <xanvial@gmail.com>
2012-04-16 00:20:42 +0700

    Properly remove improved blood presence. Fix exploit using dual talent

---
+++ sql/custom/world/improved_blood_presence.sql
@@ -0,0 +1,3 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dk_improved_blood_presence';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(-50365, 'spell_dk_improved_blood_presence');
