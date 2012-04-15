4cd29f44cf75ed6360286b721fb19ac94d59b060
Xanvial <xanvial@gmail.com>
2012-04-16 02:55:25 +0700

    Properly remove improved unholy presence and remove aura from exploit.

---
+++ sql/custom/world/improved_presence.sql
@@ -0,0 +1,7 @@
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dk_improved_blood_presence';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(-50365, 'spell_dk_improved_blood_presence');
+
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_dk_improved_unholy_presence';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(-50391, 'spell_dk_improved_unholy_presence');
