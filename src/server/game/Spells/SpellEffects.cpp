77240ba47423983c19647d4762fbb7b4f37070c2
Xanvial <xanvial@gmail.com>
2012-04-22 05:39:47 +0700

    Revert "hack fix Valkyr Shadowguard Charge to ignore first collision"
    This reverts commit d24a27abb997f4acf6b3a58f126a11b48ff60237.

--- src/server/game/Spells/SpellEffects.cpp
+++ src/server/game/Spells/SpellEffects.cpp
@@ -5501,12 +5501,10 @@ void Spell::EffectChargeDest(SpellEffIndex /*effIndex*/)
     {
         Position pos;
         destTarget->GetPosition(&pos);
-        if(m_caster->GetEntry() != 36609)       // hack fix for Valkyr Shadowguard, ignore first collision
-        {
-            float angle = m_caster->GetRelativeAngle(pos.GetPositionX(), pos.GetPositionY());
-            float dist = m_caster->GetDistance(pos);
-            m_caster->GetFirstCollisionPosition(pos, dist, angle);
-        }
+        float angle = m_caster->GetRelativeAngle(pos.GetPositionX(), pos.GetPositionY());
+        float dist = m_caster->GetDistance(pos);
+        m_caster->GetFirstCollisionPosition(pos, dist, angle);
+
         m_caster->GetMotionMaster()->MoveCharge(pos.m_positionX, pos.m_positionY, pos.m_positionZ);
     }
 }
