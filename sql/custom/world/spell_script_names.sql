ed874040f63f4374ff0b46a884330ea86f463651
Xanvial <xanvial@gmail.com>
2012-05-10 12:04:36 +0700

    Hack fix of Entrapment and Lock and Load

--- sql/custom/world/spell_script_names.sql
+++ sql/custom/world/spell_script_names.sql
@@ -49,6 +49,16 @@ DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_hun_pet_aspect_of_the
 INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
 (61669, 'spell_hun_pet_aspect_of_the_beast');
 
+-- Entrapment and Lock and Load of frost trap
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_hun_frost_trap';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(63487, 'spell_hun_frost_trap');
+
+-- Entrapment of snake trap
+DELETE FROM `spell_script_names` WHERE `ScriptName`='spell_hun_snake_trap';
+INSERT INTO `spell_script_names` (`spell_id`, `ScriptName`) VALUES 
+(45145, 'spell_hun_snake_trap');
+
 
 -- Paladin
 -- Blessing of Kings and Sanctuary
