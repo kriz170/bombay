c1248877501f787aee1eff0813c6a8ecd6718b73
krishna laptop kantor <krenz_37@yahoo.com>
2012-08-07 10:08:31 +0700

    sql arena announcer

---
+++ sql/custom/world/arena_announcer_2.sql
@@ -0,0 +1,3 @@
+UPDATE `trinity_string` SET `content_default`='|cffff0000[Arena Queue Announcer]:|r -- Joined : %ux%u |r' WHERE `entry`=718;
+
+UPDATE `trinity_string` SET `content_default`='|cffff0000[Arena Queue Announcer]:|r -- Exited : %ux%u |r' WHERE `entry`=719;
