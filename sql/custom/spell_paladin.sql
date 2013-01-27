7b7f017099153ed687838225d1978468e731346a
Xanvial <xanvial@gmail.com>
2013-01-28 06:45:53 +0700

    DB/Spells: Paladin's Daybreak proc

--- sql/custom/spell_paladin.sql
+++ sql/custom/spell_paladin.sql
@@ -40,3 +40,9 @@ DELETE FROM `spell_proc_event` WHERE `entry` IN (53569, 53576);
 INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
 (53569, 0, 10, 2097152, 65536, 0, 0, 2, 0, 0, 0),
 (53576, 0, 10, 2097152, 65536, 0, 0, 2, 0, 0, 0);
+
+-- Daybreak proc
+DELETE FROM `spell_proc_event` WHERE `entry` IN (88820,88821);
+INSERT INTO `spell_proc_event` (`entry`, `SchoolMask`, `SpellFamilyName`, `SpellFamilyMask0`, `SpellFamilyMask1`, `SpellFamilyMask2`, `procFlags`, `procEx`, `ppmRate`, `CustomChance`, `Cooldown`) VALUES 
+(88820, 0, 10, 3221225472, 0, 1024, 0, 0, 0, 0, 0),
+(88821, 0, 10, 3221225472, 0, 1024, 0, 0, 0, 0, 0);
